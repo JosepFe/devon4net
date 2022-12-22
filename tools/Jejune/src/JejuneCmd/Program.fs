122d2b0b380762204899d020ef796bbb84ac7fdb
Josep Ferrandis Jorge <josepfejo1@gmail.com>
2022-12-22 10:20:50 +0100

    Adding jejune functionality to generate component assets

--- tools/Jejune/src/JejuneCmd/Program.fs
+++ tools/Jejune/src/JejuneCmd/Program.fs
@@ -1,7 +1,6 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
-open HandlebarsDotNet
 // Define a function to construct a message to print
 
 open JejuneCmd
@@ -14,10 +13,16 @@ let main argv =
     let baseDirectory = Directory.GetParent(__SOURCE_DIRECTORY__)
     let entities_path = Path.Combine(baseDirectory.ToString(), "Entities")
     let entities = load_entities(Path.Combine(entities_path, "entities.txt"))
+
     let templates_path = Path.Combine(baseDirectory.ToString(),"Templates")
+
+    let components_path = Path.Combine(baseDirectory.ToString(), "Components")
+    let components = load_components(Path.Combine(components_path, "components.txt"))
+
     Console.Write("Output path: ")
     let source_path = Console.ReadLine()
-    copyAndExpandFiles entities entities_path  templates_path source_path
+    //copyAndExpandFiles entities entities_path  templates_path source_path
+    devon4net_webapi_components_generation components components_path templates_path source_path
     0
     
     
